fb8449e86094d8990016b65443f35e79f174f30e
Joel Mason <joel.mason@creditsafe.com>
2020-08-19 15:12:34 +0100

    init

--- client/src/components/tables/StockTable.js
+++ client/src/components/tables/StockTable.js
@@ -21,7 +21,11 @@ class StockTable extends Component {
     static getDerivedStateFromProps(props, state) {
         console.log("props", props)
         console.log("state", state)
-        if((props.tableItems.items !== 'undefined' && props.tableItems.items.items.length !== state.checkboxes.length)) {
+        if(props.tableItems.items === 'undefined') {
+            console.log("I was undefined")
+            return null
+        }
+        if(props.tableItems.items.items.length !== state.checkboxes.length) {
             if(props.tableItems.items.items.length > 0) {
                 return {
                     ...state,
