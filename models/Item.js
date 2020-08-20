0e67cfa5c7192f6ac4453139e18e63b0be5a725d
Joel Mason <joel.mason@creditsafe.com>
2020-08-20 18:03:10 +0100

    added searching

--- models/Item.js
+++ models/Item.js
@@ -14,6 +14,10 @@ const ItemSchema = new Schema({
       type: String,
       required: true
   },
+  productPrice: {
+    type: Number,
+    required: true
+  },
   lastEmailed: {
     type: Date,
     required: true
